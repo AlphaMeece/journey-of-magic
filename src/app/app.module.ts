b09a5f859e7bf7f0653044a266590b3ee94a38b3
Daemon Larson <aslplarson@gmail.com>
2022-08-07 13:27:13 -0400

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { InfoBarComponent } from './info-bar/info-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InfoBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
